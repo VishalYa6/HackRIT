e4a05258ebef37674cbcb7793e111f299911f8e9
Vinisha <dsouzatwinkle72@gmail.com>
2025-07-31 20:38:39 +0530

    services

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Card from './Card'
 import Footer from './Footer'
+import Testimonial from './Testimonial'
+import Services from './Services'
 const AboutUs = () => {
   return (
     <>
     <Card/>
+    <Services/>
+    <Testimonial/>
    <Footer/>
 
     </>
